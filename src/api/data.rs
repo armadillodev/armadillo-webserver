c843346792c06530473513b49e0730dbafa8b406
Mika Nikura <deweykai5@gmail.com>
2020-07-05 16:00:19 +0000

    cleanup

--- src/api/data.rs
+++ src/api/data.rs
@@ -1,7 +1,37 @@
+use crate::RecordsDbConn;
+
+use rocket_contrib::json::Json;
+use diesel::prelude::*;
+
 #[derive(Queryable, Serialize, Deserialize)]
 pub struct TrailorData {
     pub id: i32,
     pub trailor: i32,
     pub timestamp: i32,
     pub temperature: Option<i32>,
+}
+
+#[get("/")]
+pub fn get_all(conn: RecordsDbConn) -> Json<Vec<TrailorData>> {
+    use super::schema::trailor_data::dsl::*;
+
+    let results = trailor_data
+        .limit(100)
+        .load::<TrailorData>(&*conn)
+        .expect("Database failed");
+
+    Json(results)
+}
+
+#[get("/<id>")]
+pub fn get(conn: RecordsDbConn, id: i32) -> Option<Json<TrailorData>> {
+    use super::schema::trailor_data::dsl::*;
+
+    let results = trailor_data
+        .find(id)
+        .load::<TrailorData>(&*conn)
+        .expect("Database failed")
+        .pop()?;
+
+    Some(Json(results))
 }

f78fa6a57d2fd964a2d7b7a9ea26d20e89cbf2d5
Kai Dewey <deweykai5@gmail.com>
2020-07-15 16:31:54 +0900

    Add cascade delete to tables

--- migrations/2020-07-05-120227_create_basic_tables/up.sql
+++ migrations/2020-07-05-120227_create_basic_tables/up.sql
@@ -7,32 +7,32 @@ CREATE TABLE trailers (
     trailer_id SERIAL NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     location TEXT NOT NULL,
-    org INTEGER NOT NULL REFERENCES orgs(org_id)
+    org INTEGER NOT NULL REFERENCES orgs(org_id) ON DELETE CASCADE
 );
 
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    org INTEGER NOT NULL REFERENCES orgs(org_id),
+    org INTEGER NOT NULL REFERENCES orgs(org_id) ON DELETE CASCADE,
     first_name TEXT NOT NULL,
     last_name TEXT
 );
 
 CREATE TABLE bikes (
   bike_id SERIAL PRIMARY KEY,
-  trailer INTEGER NOT NULL REFERENCES trailers(trailer_id)
+  trailer INTEGER NOT NULL REFERENCES trailers(trailer_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_logs (
     trailer_log_id SERIAL PRIMARY KEY,
-    client INTEGER NOT NULL REFERENCES users(user_id),
-    bike INTEGER NOT NULL REFERENCES bikes(bike_id),
+    client INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    bike INTEGER NOT NULL REFERENCES bikes(bike_id) ON DELETE CASCADE ,
     time_start TIMESTAMP NOT NULL,
     time_end TIMESTAMP NOT NULL
 );
 
 CREATE TABLE trailer_data (
     trailer_data_id SERIAL,
-    trailer INTEGER NOT NULL REFERENCES trailers(trailer_id),
+    trailer INTEGER NOT NULL REFERENCES trailers(trailer_id) ON DELETE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     temperature INTEGER,
     PRIMARY KEY (trailer_data_id, created_at)
@@ -40,7 +40,7 @@ CREATE TABLE trailer_data (
 
 CREATE TABLE bike_data (
   bike_data_id SERIAL,
-  bike INTEGER NOT NULL REFERENCES bikes(bike_id),
+  bike INTEGER NOT NULL REFERENCES bikes(bike_id) ON DELETE CASCADE,
   created_at TIMESTAMP NOT NULL DEFAULT now(),
   voltage INTEGER,
   rpm INTEGER,

8a990b8061f0be60a71a31938706637c05010182
Kai Dewey <deweykai5@gmail.com>
2020-07-15 17:14:35 +0900

    add data post route

--- src/api/data.rs
+++ src/api/data.rs
@@ -4,6 +4,14 @@ use diesel::prelude::*;
 use rocket_contrib::json::Json;
 
 #[derive(Serialize, Deserialize)]
+pub struct NewBikeData {
+    pub voltage: Option<i32>,
+    pub rpm: Option<i32>,
+    pub current: Option<i32>,
+}
+
+/*
+#[derive(Serialize, Deserialize)]
 pub struct NewTrailorData {
     pub trailor: i32,
     pub temperature: Option<i32>,
@@ -34,17 +42,21 @@ pub fn get(conn: RecordsDbConn, id: i32) -> Option<Json<TrailorData>> {
 
     Some(Json(results))
 }
+*/
 
-#[post("/", format = "json", data = "<new_trailor_data>")]
-pub fn new_data(
+#[post("/<bike_id>", format = "json", data = "<new_bike_data>")]
+pub fn post_bike_data(
     conn: RecordsDbConn,
-    new_trailor_data: Json<NewTrailorData>,
+    bike_id: i32,
+    new_bike_data: Json<NewBikeData>,
 ) -> Option<&'static str> {
-    use super::schema::trailor_data::dsl::*;
-    diesel::insert_into(trailor_data)
+    use super::schema::bike_data::dsl::*;
+    diesel::insert_into(bike_data)
         .values((
-            trailor.eq(new_trailor_data.trailor),
-            temperature.eq(new_trailor_data.temperature),
+            bike.eq(bike_id),
+            voltage.eq(new_bike_data.voltage),
+            current.eq(new_bike_data.current),
+            rpm.eq(new_bike_data.rpm),
         ))
         .execute(&*conn)
         .expect("Error saving trailor data");
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,7 +1,7 @@
 mod schema;
 mod models;
 
-//mod data;
+mod data;
 //mod logs;
 
 mod orgs;
@@ -12,4 +12,5 @@ use rocket::Rocket;
 pub fn rocket(rocket: Rocket) -> Rocket {
     rocket
         .mount("/org", routes![orgs::org_list_view, orgs::org_structure_view])
+        .mount("/data/bike", routes![data::post_bike_data])
 }

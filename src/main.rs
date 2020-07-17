4f339b2e28b60d1cdced2f69d3a2fc73a17f0d03
Kai Dewey <deweykai5@gmail.com>
2020-07-18 00:50:35 +0900

    recreate data routes

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,120 @@
 // #![feature(proc_macro_hygiene, decl_macro)]
 // #![recursion_limit = "256"]
 
-// #[macro_use]
-// extern crate rocket;
-// #[macro_use]
-// extern crate rocket_contrib;
-// #[macro_use]
-// extern crate serde_derive;
-// #[macro_use]
-// extern crate diesel;
-// #[macro_use]
-// extern crate diesel_migrations;
-// #[macro_use]
-// extern crate log;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_migrations;
+#[macro_use] extern crate log;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::middleware::Logger;
-use env_logger::Env;
+use actix_web::{web, App, HttpServer, HttpResponse, Error, Responder};
+use diesel::PgConnection;
+use diesel::r2d2::{self, ConnectionManager};
 
-struct AppState {
-    name: String,
+mod db;
+
+type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+// run migrations on database
+embed_migrations!();
+
+fn run_db_migrations(pool: DbPool) -> Result<(), String> {
+    let conn = pool.get().expect("couldn't get db connection from pool");
+
+    if let Err(e) = embedded_migrations::run(&conn) {
+        return Err(format!("Failed to run database migrations: {:?}", e));
+    }
+
+    Ok(())
 }
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
+async fn get_bike_data(pool: web::Data<DbPool>, bike_id: web::Path<i32>) -> Result<impl Responder, Error> {
+    let bike_id = bike_id.into_inner();
+    let conn = pool.get().expect("couldn't get db connection from pool");
+
+    let bikes = web::block(move || db::data::find_bike_data(&conn, bike_id, 100))
+        .await
+        .map_err(|e| {
+            error!("{}", e);
+            HttpResponse::InternalServerError().finish()
+        })?;
+
+    if bikes.len() == 0 {
+        Ok(HttpResponse::NotFound().body(format!("no bike data with id: {} was found", bike_id)))
+    } else {
+        Ok(HttpResponse::Ok().json(bikes))
+    }
 }
 
-async fn index2(data: web::Data<AppState>) -> String {
-    let app_name = &data.name;
+async fn get_latest_bike_data(pool: web::Data<DbPool>, bike_id: web::Path<i32>) -> Result<impl Responder, Error> {
+    let bike_id = bike_id.into_inner();
+    let conn = pool.get().expect("couldn't get db connection from pool");
 
-    format!("Hello {}!", app_name)
+    let mut bike = web::block(move || db::data::find_bike_data(&conn, bike_id, 1))
+        .await
+        .map_err(|e| {
+            error!("{}", e);
+            HttpResponse::InternalServerError().finish()
+        })?;
+
+    if bike.len() == 0 {
+        return Ok(HttpResponse::NotFound().body(format!("no bike data with id: {} was found", bike_id)));
+    }
+    
+    Ok(HttpResponse::Ok().json(bike.pop().unwrap()))
+}
+
+async fn add_bike_data(
+    pool: web::Data<DbPool>,
+    bike_id: web::Path<i32>,
+    data: web::Json<db::data::CreateBikeData>
+) -> Result<impl Responder, Error> {
+    let bike_id = bike_id.into_inner();
+    let conn = pool.get().expect("couldn't get connection from pool");
+    let data = data.into_inner();
+
+    web::block(move || db::data::insert_new_bike_data(&conn, bike_id, data))
+        .await
+        .map_err(|e| {
+            error!("{}", e);
+            HttpResponse::InternalServerError().finish()
+        })?;
+
+    // no need to send data back
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    std::env::set_var("RUST_LOG", "actix_web=info");
 
-    HttpServer::new(|| {
+    // setup database connection pool
+    let connspec = std::env::var("DATABASE_URL")
+        .unwrap_or("postgres://postgres:postgres@localhost/armadillo".to_string());
+    let manager = ConnectionManager::<PgConnection>::new(connspec);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to build pool");
+
+
+    let bind = "localhost:8000";
+
+    println!("starting server at: {}", bind);
+
+    // run migrations
+    run_db_migrations(pool.clone()).unwrap();
+
+    // start server
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .wrap(Logger::default())
-            .data(AppState {
-                name: String::from("Actix-web")
-            })
-            .service(
-                web::scope("/app")
-                    .route("/", web::get().to(index))
-                    .route("/again", web::get().to(index2))
+            .service(web::scope("/data")
+                .route("/bike/{bike_id}", web::get().to(get_bike_data))
+                .route("/bike/{bike_id}/latest", web::get().to(get_latest_bike_data))
+                .route("/bike/{bike_id}", web::post().to(add_bike_data))
             )
     })
-    .bind("localhost:8000")?
+    .bind(&bind)?
     .run()
     .await
 }

a231a121ab835da8c7caf6e843524f0ad5badf06
Mika Nikura <deweykai5@gmail.com>
2020-07-26 22:18:28 +0900

    data route now use DataQuery

--- src/data.rs
+++ src/data.rs
@@ -4,13 +4,14 @@ use actix_web::{web, Responder, Error, HttpResponse};
 
 use crate::DbPool;
 use super::db;
+use db::DataQuery;
 
 // routes for getting bike data
 pub async fn get_bike_data(pool: web::Data<DbPool>, bike_id: web::Path<i32>) -> Result<impl Responder, Error> {
     let bike_id = bike_id.into_inner();
     let conn = pool.get().expect("couldn't get db connection from pool");
 
-    let bikes = web::block(move || db::data::find_bike_data(&conn, bike_id, 100))
+    let bikes = web::block(move || db::BikeData::find(&conn, bike_id, 100))
         .await
         .map_err(|e| {
             error!("{}", e);
@@ -28,7 +29,7 @@ pub async fn get_latest_bike_data(pool: web::Data<DbPool>, bike_id: web::Path<i3
     let bike_id = bike_id.into_inner();
     let conn = pool.get().expect("couldn't get db connection from pool");
 
-    let mut bike_data = web::block(move || db::data::find_bike_data(&conn, bike_id, 1))
+    let mut bike_data = web::block(move || db::BikeData::find(&conn, bike_id, 1))
         .await
         .map_err(|e| {
             error!("{}", e);
@@ -53,7 +54,7 @@ pub async fn add_bike_data(
     let conn = pool.get().expect("couldn't get connection from pool");
     let data = data.into_inner();
 
-    let _updated_data = web::block(move || db::data::insert_new_bike_data(&conn, bike_id, data))
+    let _updated_data = web::block(move || db::BikeData::insert(&conn, bike_id, data))
         .await
         .map_err(|e| {
             error!("{}", e);
@@ -62,7 +63,7 @@ pub async fn add_bike_data(
 
     let conn = pool.get().expect("couldn't get connection from pool");
 
-    let mut bike_data = web::block(move || db::data::find_bike_data(&conn, bike_id, 1))
+    let mut bike_data = web::block(move || db::BikeData::find(&conn, bike_id, 1))
         .await
         .map_err(|e| {
             error!("{}", e);
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -3,5 +3,12 @@ pub mod models;
 
 pub mod data;
 pub mod orgs;
+
+pub use models::{
+    BikeData,
+    OvenData,
+};
+
+pub use data::DataQuery;
 //mod logs;
 //mod users;

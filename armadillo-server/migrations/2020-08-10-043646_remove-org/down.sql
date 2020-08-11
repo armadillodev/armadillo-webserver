4920c9226709c4d6b9f418a192e592a210dc2cae
Mika Nikura <deweykai5@gmail.com>
2020-08-12 00:07:14 +0900

    fix missing down sql statements

--- armadillo-server/migrations/2020-08-10-043646_remove-org/down.sql
+++ armadillo-server/migrations/2020-08-10-043646_remove-org/down.sql
@@ -1,1 +1,9 @@
 -- This file should undo anything in `up.sql`
+ALTER TABLE trailers;
+ADD org INTEGER NOT NULL REFERENCES orgs(org_id) ON DELETE CASCADE DEFAULT 1;
+
+ALTER TABLE users
+ADD org INTEGER NOT NULL REFERENCES orgs(org_id) ON DELETE CASCADE DEFAULT 1;
+
+ALTER TABLE users;
+DROP COLUMN trailer;

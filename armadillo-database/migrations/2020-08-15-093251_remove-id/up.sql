536418012f8d746b9048cad9d7cbf0147c3834e9
Mika Nikura <deweykai5@gmail.com>
2020-08-15 22:32:36 +0900

    fix migrations

--- armadillo-database/migrations/2020-08-15-093251_remove-id/up.sql
+++ armadillo-database/migrations/2020-08-15-093251_remove-id/up.sql
@@ -25,22 +25,22 @@ DELETE FROM solar_microgrid_data a USING (
       AND a.ctid <> b.ctid;
 
 ALTER TABLE "bike_data"
-ADD CONSTRAINT "bike_data_pk" PRIMARY KEY ("bike", "created_at"),
-DROP CONSTRAINT "bike_data_pk";
+ADD CONSTRAINT "bike_data_pkey" PRIMARY KEY ("bike", "created_at"),
+DROP CONSTRAINT "bike_data_pkey";
 
 ALTER TABLE "bike_data"
 DROP "bike_data_id";
 
 ALTER TABLE "oven_data"
-ADD CONSTRAINT "oven_data_pk" PRIMARY KEY ("oven", "created_at"),
-DROP CONSTRAINT "oven_data_pk";
+ADD CONSTRAINT "oven_data_pkey" PRIMARY KEY ("oven", "created_at"),
+DROP CONSTRAINT "oven_data_pkey";
 
 ALTER TABLE "oven_data"
 DROP "oven_data_id";
 
 ALTER TABLE "solar_microgrid_data"
-ADD CONSTRAINT "solar_microgrid_data_pk" PRIMARY KEY ("solar_microgrid", "created_at"),
-DROP CONSTRAINT "solar_microgrid_data_pk";
+ADD CONSTRAINT "solar_microgrid_data_pkey" PRIMARY KEY ("solar_microgrid", "created_at"),
+DROP CONSTRAINT "solar_microgrid_data_pkey";
 
 ALTER TABLE "solar_microgrid_data"
 DROP "solar_microgrid_data_id";
